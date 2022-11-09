7b850cfdbdb44490837e1ba9b090087f920372a9
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:10:03 +0100

    Add files via upload

---
+++ m3_l1_working-with-properties/src/main/java/com/ahanchar/LsApp.java
@@ -0,0 +1,29 @@
+package com.ahanchar;
+
+import java.time.LocalDate;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ahanchar.persistence.model.Project;
+import com.ahanchar.service.IProjectService;
+
+@SpringBootApplication
+public class LsApp {
+
+    @Autowired
+    private IProjectService projectService;
+
+    public static void main(final String... args) {
+        SpringApplication.run(LsApp.class, args);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        Project project = new Project("My First Project", LocalDate.now());
+        projectService.save(project);
+    }
+}
---
+++ m3_l1_working-with-properties/src/main/java/com/ahanchar/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ahanchar.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ahanchar.persistence.model.Project;
+import com.ahanchar.persistence.repository.IProjectRepository;
+import com.ahanchar.service.IProjectService;
+
+@Service
+public class ProjectServiceImpl implements IProjectService {
+
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+}
