373006fe4648d5d4c9fee39e58a3e5b040704e91
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:30:07 +0100

    Add files via upload

---
+++ m7_l2_full-create-operation-with-thymeleaf/src/main/java/com/ahanchar/service/ITaskService.java
@@ -0,0 +1,12 @@
+package com.ahanchar.service;
+
+import java.util.Optional;
+
+import com.ahanchar.persistence.model.Task;
+
+public interface ITaskService {
+    Optional<Task> findById(Long id);
+
+    Task save(Task task);
+
+}
