9e865d43a6d9777c878f331eeab87946d36794a8
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:29:16 +0100

    Add files via upload

---
+++ m7_l1_view-resolvers-templating-engines/src/main/java/com/ahanchar/service/IProjectService.java
@@ -0,0 +1,15 @@
+package com.ahanchar.service;
+
+import java.util.Optional;
+
+import com.ahanchar.persistence.model.Project;
+
+public interface IProjectService {
+
+    Optional<Project> findById(Long id);
+
+    Project save(Project project);
+
+    Iterable<Project> findAll();
+
+}
