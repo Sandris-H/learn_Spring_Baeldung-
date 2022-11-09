a5aaab56b52a57bce0c764465c23b4927e4bf876
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:25:29 +0100

    Add files via upload

---
+++ m6_l4_decouple-controller-from-entities/src/main/java/com/ahanchar/service/IProjectService.java
@@ -0,0 +1,11 @@
+package com.ahanchar.service;
+
+import java.util.Optional;
+
+import com.ahanchar.persistence.model.Project;
+
+public interface IProjectService {
+    Optional<Project> findById(Long id);
+
+    Project save(Project project);
+}
