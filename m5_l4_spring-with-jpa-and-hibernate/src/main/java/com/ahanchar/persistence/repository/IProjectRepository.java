0733e374c778231f7241093c57e4000ceb3ed1f5
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:19:57 +0100

    Add files via upload

---
+++ m5_l4_spring-with-jpa-and-hibernate/src/main/java/com/ahanchar/persistence/repository/IProjectRepository.java
@@ -0,0 +1,13 @@
+package com.ahanchar.persistence.repository;
+
+import java.util.Optional;
+
+import com.ahanchar.persistence.model.Project;
+
+public interface IProjectRepository {
+
+    Optional<Project> findById(Long id);
+
+    Project save(Project project);
+
+}
