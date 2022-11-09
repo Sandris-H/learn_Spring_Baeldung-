ea08d788b33ff5d58c533d830e33fbc7117aa049
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:11:21 +0100

    Add files via upload

---
+++ m3_l3_logging-in-a-spring-boot-project/src/main/java/com/ahanchar/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ahanchar.service.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.ahanchar.persistence.model.Project;
+import com.ahanchar.persistence.repository.IProjectRepository;
+import com.ahanchar.service.IProjectService;
+
+@Service
+public class ProjectServiceImpl implements IProjectService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
+
+    private IProjectRepository projectRepository;
+
+    public ProjectServiceImpl(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        LOG.debug("Project Service >> Finding Project By Id {}", id);
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public Project save(Project project) {
+        LOG.debug("Project Service >> Saving Project {}", project);
+        return projectRepository.save(project);
+    }
+
+}
