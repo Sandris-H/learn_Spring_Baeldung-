815d7a4591da2bc3fed84bb3bbdfe080abae27be
Aliaksandr <91119461+Sandris-H@users.noreply.github.com>
2022-11-09 11:36:41 +0100

    Add files via upload

---
+++ m8_l4_controller-urls-requestparam/src/main/java/com/ahanchar/service/IProjectService.java
@@ -0,0 +1,17 @@
+package com.ahanchar.service;
+
+import java.util.Optional;
+
+import com.ahanchar.persistence.model.Project;
+
+public interface IProjectService {
+    Optional<Project> findById(Long id);
+
+    Project save(Project project);
+
+    Iterable<Project> findAll();
+
+    void delete(Long id);
+    
+    Iterable<Project> findByName(String name);
+}
